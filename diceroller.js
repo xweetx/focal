b518a07006c4441c9832ca08ab407f46b02f8f18
xweetx <xweet_xilenze@yahoo.com>
2023-05-21 00:13:05 -0600

    changes diceroller

--- diceroller.js
+++ diceroller.js
@@ -1,15 +1,15 @@
 // trying to see if this works
-var dbl = 0;
-function rolldice() {
-    var x = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-    var y = Math.floor(Math.random() * ((6 - 1) + 1) + 1);
-    var dicetotal = x + y;
-    $('.dice1').attr('id', "dice" + x);
-    $('.dice2').attr('id', "dice" + y);
-    if (x == y) { //<----checking if there is a double
-        dbl++; //<---increment double count
-        if(dbl%3==0) $('.out').attr('id', "jail");
-        //Now reroll the dice, but if you hit 3 doubles in a row, you get message go to jail.
-        rolldice();
-    }
-};
+const numDice = parseInt(process.argv[2]);
+
+if (isNaN(numDice) || numDice <= 0) {
+  console.log("Invalid input. Please provide a positive integer.");
+} else {
+  const results = [];
+
+  for (let i = 0; i < numDice; i++) {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    results.push(roll);
+  }
+
+  console.log(`Rolled ${numDice} dice: ${results.join(", ")}`);
+}
