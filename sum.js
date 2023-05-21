3ba03b2b665664bc2b49e85f8cb3c94f0d296587
xweetx <xweet_xilenze@yahoo.com>
2023-05-21 00:19:25 -0600

    change sum

--- sum.js
+++ sum.js
@@ -5,5 +5,5 @@ let num1 = Number(args[2]); // access for the first argument value
 let num2 = Number(args[3]); // access for the second argument value
 
 console.log({
-    sum: num1 + num2,
-})
+  sum: num1 + num2
+});
