03c0af990d6f39264a83d72c2f4b3bf644c98c58
xweetx <xweet_xilenze@yahoo.com>
2023-06-11 14:34:26 -0600

    updates

---
+++ pizza.js
@@ -0,0 +1,34 @@
+class Pizza {
+  
+  constructor(size, crust) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = ["cheese"];
+  }
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  get price() {
+    const basePrice = 10;
+    const toppingPrice = 2;
+    return basePrice + this.toppings.length * toppingPrice;
+  }
+
+  set size(size) {
+    if (size === 's' || size === 'm' || size === 'l') {
+      this._size = size;
+    }
+  }
+
+  get size() {
+    return this._size
+  }
+}
+  
+let pizza = new Pizza();
+
+console.log(`Pizza Price: ${pizza.price}`);
+pizza.size = 'm';
+console.log(pizza.size);
